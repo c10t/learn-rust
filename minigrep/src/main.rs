2405f4ae2c17e3ed082ddf0fc0bd0e0dfd5cea54
c10t <c10t@fa.ke>
2019-04-02 06:44:11 +0900

    extract run from main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
@@ -7,6 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // println!("{:?}", args);
 
+    // closure
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
@@ -15,13 +17,22 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+// Box<dyn Error> means the function will return a type that implements the Error trait
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong readinng the file");
+    f.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
