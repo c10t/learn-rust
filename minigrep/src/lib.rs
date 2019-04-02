dd8979bbd5a832aebf383652a09ee2bdc2e384d1
c10t <c10t@fa.ke>
2019-04-02 22:43:47 +0900

    search feature

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -29,7 +29,46 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
+
+// 10.3
+// The constraint we want to express in this signature is that 
+// all the references in the parameters and the return value must have the same lifetime.
+//
+// In this case, we indicate that the returned vector should contain string slices 
+// that reference slices of the argument contents (rather than the argument query).
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+   
+    results
 }
