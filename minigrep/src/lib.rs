0c2d6eca46e5e0571c3466ff3a9b30225daa1f22
c10t <c10t@fa.ke>
2019-04-03 08:09:24 +0900

    use iterator

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -11,14 +11,20 @@ pub struct Config {
 
 impl Config {
     // &'static str is the type of string literals: “The Static Lifetime” section in Chapter 10
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            // panic!("not enough arguments");
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        // ignore program name
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
@@ -87,26 +93,21 @@ Trust me.";
 // In this case, we indicate that the returned vector should contain string slices 
 // that reference slices of the argument contents (rather than the argument query).
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-   
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
+    contents.lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
+    // let query = query.to_lowercase();
+    // let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //    if line.to_lowercase().contains(&query) {
+    //        results.push(line);
+    //    }
+    // }
    
-    results
+    // results
 }
