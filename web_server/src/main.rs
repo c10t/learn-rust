c1450698ea0cd7dff088c86aace213380c90b2f8
c10t <c10t@fa.ke>
2019-04-05 23:34:17 +0900

    read requests

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
@@ -6,6 +8,17 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection Established!");
+        handle_connection(stream);
     }
+}
+
+// The `TcpStream` instance keeps track of what data it returns to us internally.
+// It might read more data than we asked for 
+// and save that data for the next time we ask for data. 
+// It therefore needs to be mut because its internal state might change;
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    // it will replace the invalid sequence with �, the U+FFFD REPLACEMENT CHARACTER. 
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
