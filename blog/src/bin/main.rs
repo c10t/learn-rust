fbacfd15232f4cf7529acbe7310a14e629d10b6a
c10t <c10t@fa.ke>
2019-04-09 23:54:02 +0900

    encoding states and behavior as types

--- blog/src/bin/main.rs
+++ blog/src/bin/main.rs
@@ -1,5 +1,6 @@
 extern crate blog;
 use blog::Post;
+use blog::Post2;
 
 fn main() {
   let mut post = Post::new();
@@ -12,4 +13,13 @@ fn main() {
 
   post.approve();
   assert_eq!("I ate a salad for lunch today", post.content());
+
+  // Encoding States and Behavior as Types
+   
+  let mut post2 = Post2::new();
+
+  post2.add_text("I ate a salad for lunch today");
+  let post2 = post2.request_review();
+  let post2 = post2.approve();
+  assert_eq!("I ate a salad for lunch today", post2.content());
 }
--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -109,6 +109,52 @@ impl State for Published {
     }
 }
 
+// Encoding States and Behavior as Types
+
+pub struct Post2 {
+    content: String,
+}
+
+pub struct DraftPost2 {
+    content: String,
+}
+
+pub struct PendingReviewPost2 {
+    content: String,
+}
+
+impl Post2 {
+    pub fn new() -> DraftPost2 {
+        DraftPost2 {
+            content: String::new(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost2 {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn request_review(self) -> PendingReviewPost2 {
+        PendingReviewPost2 {
+            content: self.content,
+        }
+    }
+}
+
+impl PendingReviewPost2 {
+    pub fn approve(self) -> Post2 {
+        Post2 {
+            content: self.content,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
