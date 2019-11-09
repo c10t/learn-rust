74bda31d85466616fcab6907fc29c488a03e6f80
c10t <c10t@fa.ke>
2019-11-09 21:47:26 +0900

    add utils*

--- cycling/bitonic-sorter/src/utils.rs
+++ cycling/bitonic-sorter/src/utils.rs
@@ -4,11 +4,33 @@ use rand_pcg::Pcg64Mcg;
 
 pub fn new_u32_vec(n: usize) -> Vec<u32> {
   let mut rng = Pcg64Mcg::from_seed([0; 16]);
-  let mut v = Vec::with_capacigy(n);
+  rng.sample_iter(&Standard).take(n).collect()
+}
 
-  for _ in 0..n {
-    v.push(rng.sample(&Standard));
+pub fn is_sorted_ascending<T: Ord>(x: &[T]) -> bool {
+  x.windows(2).all(|pair| pair[0] <= pair[1])
+}
+
+pub fn is_sorted_descending<T: Ord>(x: &[T]) -> bool {
+  x.windows(2).all(|pair| pair[0] >= pair[1])
+}
+
+mod tests {
+  use crate::utils::{new_u32_vec, is_sorted_ascending, is_sorted_descending};
+  use crate::SortOrder::{Ascending, Descending};
+  use crate::third::sort;
+
+  #[test]
+  fn sort_u32_large() {
+    {
+      let mut x = new_u32_vec(65536);
+      assert_eq!(sort(&mut x, &Ascending), Ok(()));
+      assert!(is_sorted_ascending(&x))
+    }
+    {
+      let mut x = new_u32_vec(65536);
+      assert_eq!(sort(&mut x, &Descending), Ok(()));
+      assert!(is_sorted_descending(&x))
+    }
   }
-
-  v
 }
